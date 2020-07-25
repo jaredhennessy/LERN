dc6fd091bea4bdfb3f1b5930d25f5dfdaba04d5f
Chris Bragg <cbragg9@yahoo.com>
2020-07-25 11:50:30 -0700

    bcrypt hash, compare

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const coursesRoutes = require("./courses");
 
 router.use("/users", userRoutes);
+router.use("/courses", coursesRoutes);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
-const usersController = require("../../controller/usersController")
+const usersController = require("../../controller/usersController");
 
 // Matches with "/api/users"
 router.route("/")
-  .get(usersController.find)
-  .post(usersController.create)
+  .get(usersController.findAll)
+  
+// Matches with "/api/users/create"
+router.route("/register")
+  .post(usersController.createUser)
+
+// Matches with "/api/users/login"  
+router.route("/login")
+  .post(usersController.userLogin)
 
 module.exports = router;
