1f58bc587a0205e14961cc9baabba4569603f539
Chris Bragg <cbragg9@yahoo.com>
2020-07-25 09:53:16 -0700

    gitignore REST client

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 
-// Book routes
 router.use("/users", userRoutes);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const usersController = require("../../controller/usersController")
 
-// Matches with "/api/books"
+// Matches with "/api/users"
 router.route("/")
   .get(usersController.find)
   .post(usersController.create)
