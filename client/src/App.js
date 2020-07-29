493809841941f93ed1f7f3c7bb88c7fbcf7e48a7
Dan Weikart <danmweikart@gmail.com>
2020-07-28 22:28:17 -0700

    started Router, Navbar, Card.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Categories from "./pages/Categories";
+import Teach from "./pages/Teach";
+import Donate from "./pages/Donate";
+import EditProfile from "./pages/EditProfile";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Dashboard from "./pages/UserDashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route exact path="/about" component={About} /> */}
+        <Route exact path="/categories" component={Categories} />
+        {/* <Route exact path="/teach" component={Teach} /> */}
+        {/* <Route exact path="/donate" component={Donate} /> */}
+        {/* <Route exact path="/editprofile" component={EditProfile} /> */}
+        {/* <Route exact path="/login" component={Login} /> */}
+        {/* <Route exact path="/registration" component={Registration} /> */}
+        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "material-ui/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+        main: blue[500],
+    },
+    secondary: {
+      main: '#707070',
+    },
+  },
+});
