e14192122c69a254e67322d845f4e943812d57a0
Dan Weikart <danmweikart@gmail.com>
2020-07-30 14:13:44 -0700

    updated Categories

--- client/src/pages/Categories/index.js
+++ client/src/pages/Categories/index.js
@@ -1,12 +1,45 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import CourseCard from "../../components/CourseCard";
-import Navbar from "../../components/Navbar";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 export default function Categories() {
     return (
         <Container>
-            {/* <Navbar /> */}
+            <h1>Categories</h1>
+            
+            <div>
+      <Grid container spacing={3}>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+        <Grid item md={3}>
+          <Paper ><CourseCard/></Paper>
+        </Grid>
+      
+      </Grid>
+    </div>
+
+
             <CourseCard />
         </Container>
     );
