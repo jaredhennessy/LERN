2de196ed93d6965c793941296e7def039aaab9be
Dan Weikart <danmweikart@gmail.com>
2020-07-30 18:13:21 -0700

    added API.js in utils folder

--- client/src/pages/Categories/index.js
+++ client/src/pages/Categories/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "@material-ui/core/Container";
 import CourseCard from "../../components/CourseCard";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import API from "../../utils/API";
 
 export default function Categories() {
+
+const [courses, setCourses] = useState([])
+
+useEffect(() =>
+loadCourses()
+, [])
+
+    //loads all courses
+    function loadCourses() {
+        API.getCourses()
+        .then(res => 
+            setCourses(res.data)
+          )
+          .catch(err => console.log(err));
+    };
+
+
   return (
     <Container>
       <h1>Categories</h1>
@@ -13,7 +31,7 @@ export default function Categories() {
         <Grid container spacing={3}>
           <Grid item md={3}>
             <Paper>
-              <CourseCard />
+              <CourseCard {...courses}/>
             </Paper>
           </Grid>
           <Grid item md={3}>
