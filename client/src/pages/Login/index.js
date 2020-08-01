d68fb9fc20eed886e942c4d11a8d61503b02d7a2
Chris Bragg <cbragg9@yahoo.com>
2020-08-01 10:17:58 -0700

    login register styles, alerts

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -3,8 +3,20 @@ import { useHistory } from "react-router-dom";
 import Axios from "axios";
 import UserContext from "../../UserContext/UserContext";
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function Login() {
+  const classes = useStyles();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { setUserData } = useContext(UserContext);
@@ -12,6 +24,7 @@ function Login() {
 
   const submitForm = async (e) => {
     e.preventDefault();
+    if (!password || !username) return alert("Missing required fields.");
     Axios.post("/api/users/login", {
       username: username,
       password: password
@@ -33,12 +46,10 @@ function Login() {
   return (
     <div>
       <h1>Login Page</h1>
-      <form onSubmit={submitForm}>
-        {/* <label htmlFor="usernameInput">Username:</label><br /> */}
-        <TextField required defaultValue="Username" variant="outlined" label="Required" id="outline-required" name="usernameInput" onChange={e => setUsername(e.target.value)}/><br />
-        <label htmlFor="passwordInput">Password</label><br />
-        <TextField type="password" variant="outlined" id="outlined-password-input" label="Password" autoComplete="current-password" name="passwordInput" onChange={e => setPassword(e.target.value)}/> <br />
-        <input type="submit" />
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={submitForm}>
+        <TextField required variant="outlined" label="Username" id="outline-required" name="usernameInput" onChange={e => setUsername(e.target.value)}/><br />
+        <TextField required type="password" variant="outlined" id="outlined-password-input" label="Password" autoComplete="current-password" name="passwordInput" onChange={e => setPassword(e.target.value)}/> <br />
+        <Button variant="contained" color="primary" type="submit">Submit</Button>
       </form>
     </div>
   )
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Axios from "axios";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function Register() {
+  const classes = useStyles();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -29,16 +41,12 @@ function Register() {
   return (
     <div>
       <h1>Registration Page</h1>
-      <form onSubmit={submitForm}>
-        <label htmlFor="usernameInput">Username:</label><br />
-        <input type="text" id="usernameInput" name="usernameInput" onChange={e => setUsername(e.target.value)}/><br />
-        <label htmlFor="emailInput">Email</label><br />
-        <input type="text" id="emailInput" name="emailInput" onChange={e => setEmail(e.target.value)}/> <br />
-        <label htmlFor="passwordInput">Password</label><br />
-        <input id="passwordInput" name="passwordInput" type="password" onChange={e => setPassword(e.target.value)}/> <br />
-        <label htmlFor="passwordCheckInput">Verify Password</label><br />
-        <input id="passwordCheckInput" name="passwordCheckInput" type="password" onChange={e => setPasswordCheck(e.target.value)}/> <br />
-        <input type="submit" />
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={submitForm}>
+        <TextField required variant="outlined" label="Username" id="outline-required" name="usernameInput" onChange={e => setUsername(e.target.value)} /><br />
+        <TextField required variant="outlined" label="Email" id="outline-required" name="usernameInput" onChange={e => setEmail(e.target.value)} /><br />
+        <TextField required type="password" variant="outlined" id="outlined-password-input" label="Password" autoComplete="current-password" name="passwordInput" onChange={e => setPassword(e.target.value)} /><br />
+        <TextField required type="password" variant="outlined" id="outlined-password-input" label="Verify Password" autoComplete="current-password" name="passwordInput" onChange={e => setPasswordCheck(e.target.value)} /><br />
+        <Button variant="contained" color="primary" type="submit">Submit</Button>
       </form>
     </div>
   )
