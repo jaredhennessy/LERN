33a51612fa53e943456e7125d071b0a15135fbae
jaredhennessy <jared.hennessy@gmail.com>
2020-08-02 15:27:02 -0700

    category, course, and page routes

--- controller/coursesController.js
+++ controller/coursesController.js
@@ -1,8 +1,43 @@
-const db = require("../models");
+const mongoose = require("mongoose");
+const { Course } = require("../models");
 
 module.exports = {
   findAll: function (req, res) {
-    db.Course.find(req.query)
+    Course.find(req.query)
+      .sort({ title: 1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  new: function (req, res) {
+    const newCourse = new Course(req.body);
+    Course.collection.insertOne(newCourse)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  update: function (req, res) {
+    const id = mongoose.Types.ObjectId(req.params.id);
+    Course.findOneAndUpdate({ _id: id }, req.body, { new: true })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  findById: function (req, res) {
+    Course.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  findByCategory: function (req, res) {
+    Course.find({ category: req.params.id })
+      .sort({ title: 1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  findByInstructor: function (req, res) {
+    Course.find({ instructor: req.params.id })
       .sort({ title: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
--- controller/pagesController.js
+++ controller/pagesController.js
@@ -1,10 +1,45 @@
-const db = require("../models");
+const mongoose = require("mongoose");
+const { Page } = require("../models");
 
 module.exports = {
     findAll: function (req, res) {
-        db.Page.find(req.query)
+        Page.find(req.query)
             .sort({ title: 1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-    }
+    },
+
+    new: function (req, res) {
+        const newPage = new Page(req.body);
+        Page.collection.insertOne(newPage)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    update: function (req, res) {
+        const id = mongoose.Types.ObjectId(req.params.id);
+        Page.findOneAndUpdate({ _id: id }, req.body, { new: true })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findById: function (req, res) {
+        Page.findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findByCourse: function (req, res) {
+        Page.find({ course: req.params.id }, req.body)
+            .sort({ pageNumber: 1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findByCoursePage: function (req, res) {
+        Page.find({ course: req.params.id, pageNumber: req.params.page })
+            .sort({ title: 1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
 };
