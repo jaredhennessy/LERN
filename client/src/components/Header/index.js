1b604120a61f1bfc05ce06c49aeef3a81a29f057
Cris M <cristian.montenegro.p@gmail.com>
2020-08-02 20:44:47 -0700

    Header component finished except for the avatar login state change

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,50 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+// import { positions } from '@material-ui/system';
+import Avatar from '@material-ui/core/Avatar';
+// import { urlencoded } from "body-parser";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+    },
+    large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+
+    },
+    banner: {
+        width: "inherit",
+        height: "inherit",
+        zIndex: 0,
+    },
+    avatar: {
+        position: "fixed",
+        right: "0.8rem",
+        top: "0.8rem",
+        zIndex: 1,
+    },
+    parent: {
+        width: "100%",
+        height: "100%"
+    }
+}));
 
 function Header() {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            {/* <a href="index.html" className="brand-logo"> */}
-            <img id="logo-img" alt="banner" src={require("../../images/banner.jpg")} />
-            {/* </a> */}
+        <div className={classes.parent}>
+            <img className={classes.banner} alt="banner" src={require("../../images/banner.jpg")} />
+            <Avatar alt="Remy Sharp" className={classes.large, classes.avatar} src={require("../../images/1.jpg")} />
         </div>
     )
 }
