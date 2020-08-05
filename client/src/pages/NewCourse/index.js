4ff3fc7834ed76a55b07817e7ee730b84d024174
Cris M <cristian.montenegro.p@gmail.com>
2020-08-04 20:01:26 -0700

    newCourse Progress

--- client/src/pages/NewCourse/index.js
+++ client/src/pages/NewCourse/index.js
@@ -1,50 +1,140 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+    flexGrow: 1,
   },
-  title: {
-
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   },
-  text: {
-    width: "60%",
-    textAlign: "center",
-    position: "relative",
-    top: "0.8rem",
-    left: "20%",
-    right: "20%",
-    // bottom: "4rem",
-  },
-
 }));
 
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     '& > *': {
+//       margin: theme.spacing(1),
+//     },
+//   },
+//   title: {
 
-function Teach() {
+//   },
+//   text: {
+//     width: "60%",
+//     textAlign: "center",
+//     position: "relative",
+//     top: "0.8rem",
+//     left: "20%",
+//     right: "20%",
+//     // bottom: "4rem",
+//   },
+
+// }));
+
+
+function NewCourse() {
 
   const classes = useStyles();
 
+  // const [username, setUsername] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [passwordCheck, setPasswordCheck] = useState("");
+  // const history = useHistory();
+
+
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    // Validate required fields, password length, passwords match
+    // if (!email || !password || !passwordCheck || !username) return alert("Missing required fields.");
+    // if (password.length < 6) return alert("Password needs to be at least 6 characters.");
+
+    // const newCourse = { username, email, password, passwordCheck };
+    // const registerResponse = await Axios.post("/api/users/register", newUser).catch(err => alert(err.response.data));
+    // if (registerResponse) {
+    //   alert(`Successfully registered ${username}, please log in.`)
+    //   history.push("/login");
+    // }
+  }
+
   return (
     <div>
-      <br />
-      <h1>Course Form</h1>
+      <h1>New Course</h1>
+      <form noValidate className={classes.root} autoComplete="off" onSubmit={submitForm}>
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <Grid container spacing={0}>
+              <Grid item xs={4} >
+                <h5>Course name:</h5>
+              </Grid>
+              <Grid item xs={8}>
+                <TextField required variant="outlined" margin="dense" label="Course name" id="outline-required" name="courseNameInput" />
+              </Grid>
+            </Grid>
+          </Grid>
 
-      <p className={classes.text}>
-        In LERN, we believe in powerful education for all, no compromises, no fees…
-      </p>
-      {/* <br /> */}
-      <p className={classes.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+          <Grid item xs={6}>
+            <Grid container spacing={0}>
+              <Grid item xs={6}>
+                <h5>Category:</h5>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField required type="password" variant="outlined" id="outlined-password-input" label="browse through categories" name="selectedCategory" />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <h5>Select main course image</h5>
+            <Button variant="contained" name="browseImage" color="primary" type="submit" >Browse</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <h5>Course description</h5>
+            <TextareaAutosize required aria-label="minimum height" rowsMin={14} variant="outlined" placeholder="max 50 characters" name="newCourseDescription" />
+          </Grid>
+
+          <Grid item xs={6}>
+            <h5>Brief course description</h5>
+            <TextareaAutosize required aria-label="minimum height" rowsMin={5} variant="outlined" placeholder="max 200 characters" name="Course Description" />
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button variant="contained" color="primary" type="submit" >Submit</Button>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          <h5>Add additional material</h5>
+          <Button variant="contained" name="addMaterial" color="primary" type="submit" >Browse</Button>
+          <br />
+          <h5>show list of uploaded files</h5>
+        </Grid>
+
+
+        {/* 2nd Row!!! */}
+
+
+
+        <br />
+
+      </form>
+
+
+
       <br />
-      {/* <Button variant="contained" color="primary" type="submit" onClick={<}>I agree to the terms</Button> */}
-      <br />
-      {/* <Button variant="contained" color="primary" type="submit" onClick={<Home />}>I decline, take me back</Button> */}
+
       <br />
 
     </div>
@@ -52,4 +142,4 @@ function Teach() {
   )
 }
 
-export default Teach;
+export default NewCourse;
