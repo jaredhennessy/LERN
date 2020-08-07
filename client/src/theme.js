4f1518a0cc1a16c9cc3b9aa26b9ce109c4ca152f
Dan Weikart <danmweikart@gmail.com>
2020-08-06 21:20:38 -0700

    created global color theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,9 +1,9 @@
-import { createMuiTheme } from "material-ui/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
 
 export default createMuiTheme({
   palette: {
     primary: {
-        main: blue[500],
+        main: "#2294E3",
     },
     secondary: {
       main: '#707070',
