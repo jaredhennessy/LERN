debd316ac906b8545228d4f41a13df247c5c3f7d
jaredhennessy <jared.hennessy@gmail.com>
2020-08-11 19:14:17 -0700

    LERN images now display page images rather than course images

--- client/src/pages/LERN/index.js
+++ client/src/pages/LERN/index.js
@@ -7,7 +7,7 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+// import CardMedia from "@material-ui/core/CardMedia";
 import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
