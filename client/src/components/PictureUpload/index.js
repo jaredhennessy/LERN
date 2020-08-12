0fcea22245121e14a35606c48462baf42fb92d01
Chris Bragg <cbragg9@yahoo.com>
2020-08-11 18:23:49 -0700

    revert user profile

--- client/src/components/PictureUpload/index.js
+++ client/src/components/PictureUpload/index.js
@@ -2,48 +2,18 @@ import React, { useState, useContext } from "react";
 import axios from "axios";
 import UserContext from "../../UserContext/UserContext";
 import Button from "@material-ui/core/Button";
-import AddIcon from "@material-ui/icons/Add";
 import InputLabel from "@material-ui/core/InputLabel";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   autoMargin: {
-    margin: "auto",
+    margin: "auto"
   },
-
-  inputfile: {
-    width: "0.1px",
-    height: "0.1px",
-    opacity: 0,
-    overflow: "hidden",
-    position: "absolute",
-    zIndex: -1,
-    fontSize: "1.25em",
-    fontWeight: 700,
-    color: "white",
-    backgroundColor: "black",
-    display: "inline-block",
-    cursor: "pointer",
-  },
-
-//   label: {
-//     fontSize: "1.25em",
-//     fontWeight: 700,
-//     color: "white",
-//     backgroundColor: "black",
-//     display: "inline-block",
-//     cursor: "pointer",
-//     // pointerEvents: "none",
-// },
-
-}));
-
-
+}))
 
 function PictureUpload() {
   const { userData } = useContext(UserContext);
   const [file, setFile] = useState(null);
-
   const classes = useStyles();
 
   async function postAndUpdate(event) {
@@ -51,10 +21,7 @@ function PictureUpload() {
 
     // Post form to database, response returns the filename
     let formData = new FormData();
-
     formData.append("file", file);
-    console.log(file);
-    console.log(formData);
     const fileUpload = await axios({
       method: "post",
       url: "/api/files/upload",
@@ -70,65 +37,37 @@ function PictureUpload() {
         imageURL: fileName,
       })
       .then(() => {
-        // window.location.reload();
+        window.location.reload();
       });
-      
   }
 
   return (
-
-    //original
     <div>
       <form onSubmit={event => postAndUpdate(event)}>
         <div>
-          <label data-button className={classes.inputfile} htmlFor="file">Choose File</label>
-          <br />
-          <input 
-          // className="MuiButton-label MuiButton-containedPrimary MuiButton-root MuiButtonBase-root"
-           type="file" name="file" id="file" onChange={e => setFile(e.target.files[0])} />
+          <InputLabel htmlFor="file">
+            <br />
+            <input
+              className={classes.autoMargin}
+              type="file"
+              name="file"
+              id="file"
+              onChange={e => setFile(e.target.files[0])}
+            />
+          </InputLabel >
         </div>
-        <input className="MuiButton-label MuiButton-containedPrimary MuiButton-root MuiButtonBase-root" type="submit" value="Submit" />
+        <br />
+        <Button
+          className={classes.autoMargin}
+          variant="contained"
+          color="primary"
+          type="submit"
+          value="Submit"
+        >
+          Submit
+        </Button>
       </form>
     </div>
-
-    //  buttons only
-    // <div>
-    //   <form onSubmit={event => postAndUpdate(event)}>
-    //     <div>
-    //       <InputLabel htmlFor="file">
-    //         <br />
-    //         <input
-    //           style={{ display: "none" }}
-    //           type="file"
-    //           name="file"
-    //           id="file"
-    //           onChange={e => setFile(e.target.files[0])}
-    //         />
-    //         <span></span>
-    //         <Button
-    //           className={classes.autoMargin}
-    //           color="primary"
-    //           size="small"
-    //           component="span"
-    //           aria-label="add"
-    //           variant="contained"
-    //         >
-    //           <AddIcon /> Upload photo
-    //         </Button>
-    //       </InputLabel >
-    //     </div>
-    //     <br />
-    //     <Button
-    //       className={classes.autoMargin}
-    //       variant="contained"
-    //       color="secondary"
-    //       type="submit"
-    //       value="Submit"
-    //     >
-    //       Submit
-    //     </Button>
-    //   </form>
-    // </div>
   );
 }
 
